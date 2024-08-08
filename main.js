9f2d8085121baa3ae8257368a3282241204f2e7f
shrelhan <shrelhan.relhan@gmail.com>
2024-08-09 04:03:29 +0530

    new project

---
+++ main.js
@@ -0,0 +1,9 @@
+let modal = document.getElementById("modal");
+
+function openModal() {
+  modal.classList.add("open-modal");
+}
+
+function closeModal() {
+  modal.classList.remove("open-modal");
+}
